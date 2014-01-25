c9f428d249d6de4f6006b1f31125ca8e35bdac91
JessicaGarson <jessica.garson@gmail.com>
2014-01-25 13:48:23 -0500

    Learning Python
    Fun learning python

---
+++ brain.py
@@ -0,0 +1,3 @@
+#!\usr\bin\python
+print('The Bright Side ' + 'of Life....') 
+#+ means concatencate for strings
---
+++ script1.py
@@ -0,0 +1,5 @@
+import sys #load a libary module
+print(sys.platform)
+print(2 ** 32) #raise 2 to a power 
+x = 'Spam!'
+print (x * 8) #string repitition 
