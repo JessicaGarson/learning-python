20f7a6f105d8aa151dde238ff877cf2ddd9fb87e
JessicaGarson <jessica.garson@gmail.com>
2014-01-26 12:51:15 -0500

    I'm using the command line.

---
+++ raw_imput.py
@@ -0,0 +1,9 @@
+print "Hello what is your name?",
+name = raw_input()
+print "Where are you from?",
+city = raw_input()
+print "Nice to meet you!, when can I see you again"
+time_to_see_you = raw_input()
+
+print "So your name is %s, you are from %s and I will see you %s" % (
+	name, city, time_to_see_you)
